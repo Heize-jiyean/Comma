d62741fa97f573425da5176b92cc40339423c70a
Jiyun Jeong <wjdwldbsskfk@gmail.com>
2024-07-11 15:14:09 +0900

    비밀번호 유효성 검사 추가

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
     // 출생연도에 1950년 이후 넣기
     const birthYearSelect = document.getElementById('birth-year');
@@ -58,7 +58,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     // 의사 클릭 시
-    doctorRadio.addEventListener('change', function() {
+    doctorRadio.addEventListener('change', function () {
         if (doctorRadio.checked) {
             removeInfo();
             addDoctorInfo();
@@ -66,7 +66,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     // 환자 클릭 시
-    patientRadio.addEventListener('change', function() {
+    patientRadio.addEventListener('change', function () {
         if (patientRadio.checked) {
             removeInfo();
             addPatientInfo()
@@ -75,6 +75,21 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // 닉네임 입력 시 유효성 검사 진행
     document.getElementById('nickname').addEventListener('blur', checkNickname);
+
+    // 폼 제출 시 유효성 검사 진행
+    const form = document.getElementById('form');
+    form.addEventListener('submit', async function (event) {
+        console.log("되고있나요?")
+        event.preventDefault(); // 기본 동작(페이지 새로고침)을 막습니다.
+    
+        const passwordValid = checkPassword();
+        const eamilValid = await sendAuthEmail();
+    
+        if (passwordValid && eamilValid) {
+            console.log("됐다~")
+            // form.submit();
+        }
+    });
 });
 
 // 이메일 중복 확인
@@ -95,6 +110,7 @@ async function checkEmailDuplicate(email) {
 // 인증 이메일 보내기
 async function sendAuthEmail() {
     const email = document.getElementById("email").value;
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     const emailError = document.getElementById("email_error")
 
     // 이메일 입력 확인
@@ -102,13 +118,21 @@ async function sendAuthEmail() {
         emailError.textContent = '이메일을 입력해 주세요.';
         emailError.style.display = 'inline';
         return false;
+    } else if (!emailPattern.test(email)) {
+        emailError.textContent = '이메일 형식이 올바르지 않습니다.';
+        emailError.style.display = 'inline';
+        return false;
     }
-
     // 이메일 중복 확인
-    if (await checkEmailDuplicate(email)) {
+    else if (await checkEmailDuplicate(email)) {
         emailError.textContent = '사용 중인 이메일입니다.';
         emailError.style.display = 'inline';
         return false;
+    }
+    // 인증 이메일 전송 (이메일 전송 현재 미 구현)
+    else {
+        emailError.style.display = 'none';
+        return true;
     }
 
 }
@@ -145,5 +169,31 @@ async function checkNickname() {
         nicknameError.textContent = '사용 중인 닉네임입니다.';
         nicknameError.style.display = 'inline';
         return false;
+    }
+}
+
+// 비밀번호 유효성 검사
+function checkPassword() {
+    const password = document.getElementById("password").value;
+    const passwordError = document.getElementById("password_error")
+    const confirm_password = document.getElementById("confirm_password").value;
+    const confirm_passwordError = document.getElementById("password_confirm_error");
+
+    // 비밀번호 입력 확인
+    if (!password) {
+        passwordError.textContent = '비밀번호를 입력해 주세요.'
+        passwordError.style.display = 'inline';
+        return false;
+    }
+    // 비밀번호 재확인 입력 확인
+    else if (password != confirm_password) {
+        confirm_passwordError.textContent = '비밀번호가 일치하지 않습니다.'
+        confirm_passwordError.style.display = 'inline';
+        return false
+    }
+    else {
+        passwordError.style.display = 'none';
+        confirm_passwordError.style.display = 'none';
+        return true
     }
 }
