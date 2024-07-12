47fb7e4c2f1bfa4ff4739366b20ebda208ab3060
crolvlee <leecrolv@gmail.com>
2024-07-12 14:35:45 +0900

    feat: 프로필 카드 컴포넌트 생성

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,7 +1,12 @@
 const profileModel = require('../models/User');
 
 // 환자 프로필 페이지 반환
-exports.patientProfilePage = () => {}
+exports.patientProfilePage = (req, res) => {
+    res.render("profile/patient.ejs", { user: req.user || null });
+}
 
 // 상담사 프로필 페이지 반환
-exports.counselorProfilePage = () => {}
+exports.counselorProfilePage = (req, res) => {
+    res.render("profile/counselor.ejs", { user: req.user || null });
+}
+
--- routers/profileRouter.js
+++ routers/profileRouter.js
@@ -2,10 +2,14 @@ const express = require("express");
 const router = express.Router();
 const profileController = require("../controllers/profileController");
 
-// 환자 프로필 페이지 반환
-router.get('/patient/:patientId', profileController.patientProfilePage);
+// 테스트
+router.get('/patient/', profileController.patientProfilePage);
+router.get('/counselor/', profileController.counselorProfilePage);
 
-// 상담사 프로필 페이지 반환
-router.get('/counselor/:counselorId', profileController.counselorProfilePage);
+// // 환자 프로필 페이지 반환
+// router.get('/patient/:patientId', profileController.patientProfilePage);
+
+// // 상담사 프로필 페이지 반환
+// router.get('/counselor/:counselorId', profileController.counselorProfilePage);
 
 module.exports = router;
