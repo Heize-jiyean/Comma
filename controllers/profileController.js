eb0c4db38c6bb443ac902ed0c43d6094ef3d4f4e
crolvlee <leecrolv@gmail.com>
2024-07-14 22:56:05 +0900

    feat: 프로필 대시보드 각종 버튼 추가

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -2,7 +2,7 @@ const UserModel = require('../models/User');
 
 // 환자 프로필 페이지 반환
 exports.patientProfilePage = async (req, res) => {
-    const loginId = "gildong";  // TODO: 세션 처리
+    const loginId = "dain09";  // TODO: 세션 처리
     res.locals.loginId = loginId;
     const patientNickname = req.params.patientNickname;
 
@@ -12,7 +12,7 @@ exports.patientProfilePage = async (req, res) => {
             res.render("/");    // TODO: 없는 환자인 경우 띄울 페이지
             return;
         }
-        res.render("profile/patient.ejs", { patientUser: patientUser, type: 'patient' })
+        res.render("profile/patient.ejs", { patientUser: patientUser, type: 'patient'})
     } catch {
         console.error("환자 프로필 반환 오류:", error);
         res.status(500).send("서버 오류가 발생했습니다.");
