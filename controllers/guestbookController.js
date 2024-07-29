0bb0ed60c4c5f0230d6d3abe7d7355ce9e450aa8
crolvlee <leecrolv@gmail.com>
2024-07-29 15:11:35 +0900

    feat: 방명록 작성 페이지 권한 설정

--- controllers/guestbookController.js
+++ controllers/guestbookController.js
@@ -1,14 +1,36 @@
 const GuestbookModel = require('../models/Guestbook');
 const UserModel = require('../models/User'); 
+const AccessCheck = require('../utils/authUtils');
 
 // 방명록 작성 페이지 반환
 exports.new = async (req, res) => {
+    // 로그인하지 않은 사용자가 접근할 경우
+    if (!AccessCheck.isUserAuthenticated(req.session.user)) {
+        const referer = req.get('Referer') || '/';
+        return res.status(403).send(`<script>alert("권한이 없습니다."); window.location.href = "${referer}";</script>`);
+    }
+
+    // 로그인한 사용자
+    const loginRole = req.session.user.role;
+
     try {
         // 환자 정보 가져오기
         const patientId = req.params.patientId; 
         const patientUser = await UserModel.getPatientByUserId(patientId); 
 
-        res.render('guestbook/new.ejs', { patientUser: patientUser });
+        // 없는 환자일 경우
+        if (!patientUser) {
+            res.render("/");    // TODO: 없는 환자인 경우 띄울 페이지
+            console.log("없는 환자임");
+        }
+
+        if (loginRole === 'counselor') {
+            res.render('guestbook/new.ejs', { patientUser: patientUser });
+        } else {
+            res.status(403).send("접근 권한이 없습니다.");
+        }
+
+        
     } catch (error) {
         console.error("일기 작성 페이지 반환 오류:", error);
         res.status(500).send("서버 오류가 발생했습니다.");
@@ -18,7 +40,6 @@ exports.new = async (req, res) => {
 // 방명록 작성 내용 등록
 exports.register = async (req, res) => {
     const loginId = req.session.user.id;
-    // const loginRole = req.session.user.role;
 
     try {
         const { guestbookData } = req.body;
